640e2abe4b21cfffccec85ecffd152eba7b9d14f
larswarnken <lars.warnken@protonmail.com>
2020-10-23 21:47:40 +0200

    added random texts

--- app.js
+++ app.js
@@ -5,20 +5,68 @@ document.addEventListener('DOMContentLoaded', () => {
         square.addEventListener('click', clickOutcome)
     })
 
-    
+    // all squares
     const squareArray = Array.from(squares)
 
+    // array for square click status
     var isClicked = new Array()
 
+    // adds a click status for every square
     for (var i = 0; i < squareArray.length; i++) {
         isClicked[i] = false
     }
 
+    // alle texte
+    var texts = [
+        'shut up chat', 
+        'dude', 
+        'top d only', 
+        'von dono gefrontet', 
+        'ich les alle subs', 
+        'sorry leute heute leider kein', 
+        'bullshit',  'i guess',  
+        'ich war nur ... minuten zu spät', 
+        'no way',  
+        'den link skippen wir',  
+        'lösch dich',  
+        'was ist los babe',  
+        '...ist trash',  
+        'front gegen mods',  
+        'ufo bester deutscher rapper',  
+        'irgendwas mit basti',  
+        'die weiber wa, alle gleich',  
+        'i dont know',  
+        'heute nur 3 songs',  
+        'dickes papaL',  
+        'milki wird getreten',  
+        'realtalk chat',  
+        'david dobrik',
+        'melonen golem dono',
+        'der reddit ist shit',
+        'earrape',
+        'über 100€ top d'
+    ]
 
+
+    // sets random text from texts array
+    for (var i = 0; i < squareArray.length; i++) {
+        var random = Math.floor(Math.random() * texts.length)
+        squares[i].innerHTML = texts[random];
+        texts.splice(random, 1);
+
+    }
+    squares[12].innerHTML = 'X';
+    
+    
+
+
+
+    // when square is clicked
     function clickOutcome(e) {
         const index = squareArray.indexOf(e.target)
         console.log(index)
 
+        // changes bg color
         if (isClicked[index] == false) {
             isClicked[index] = true;
             squares[index].style.backgroundColor = 'red';
